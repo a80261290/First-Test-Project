b1dae76b32a0dfa90e7f58353af45cfcb0cec30e
patrick zimba <khonje@msn.com>
2011-11-07 17:37:55 +0000

    added src directory

---
+++ src/file1.php
@@ -0,0 +1,1 @@
+<?php if(arg(0)=='taxonomy' & arg(1)=='term' & arg(2)== '10'): ?>  This is where you put the html you want to print out  on this page! It won't print this if we aren't on this  page.<?php endif; ?>
